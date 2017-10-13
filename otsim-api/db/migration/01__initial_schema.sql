7a21793bdb71d65ab5d2503cb2f6fe64e27d87f0
Stelios Topkaroglou <topkas@gmail.com>
2017-10-13 14:31:37 +0300

    Configure database, initial schema and flywaydb

---
+++ otsim-api/db/migration/01__initial_schema.sql
@@ -0,0 +1,10 @@
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(45) NOT NULL,
+  `password` varchar(128) NOT NULL,
+  `fisrt_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `application_name` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username_UNIQUE` (`username`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
