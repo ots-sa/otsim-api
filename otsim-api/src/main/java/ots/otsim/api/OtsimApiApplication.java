1ee9f8c24a70440f39fe4c8a09205d4bba336145
Stelios Topkaroglou <topkas@gmail.com>
2017-10-13 12:44:40 +0300

    Initial project Push

---
+++ otsim-api/src/main/java/ots/otsim/api/OtsimApiApplication.java
@@ -0,0 +1,12 @@
+package ots.otsim.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OtsimApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OtsimApiApplication.class, args);
+	}
+}
---
+++ otsim-api/src/test/java/ots/otsim/api/OtsimApiApplicationTests.java
@@ -0,0 +1,16 @@
+package ots.otsim.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class OtsimApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
